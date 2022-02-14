eb94a4d8cf1f919bb9f3be28b6ec720511dd53fc
unknown <jurjani.valles@gmail.com>
2022-02-14 11:00:38 +0800

    Added the About page

--- jv-app/src/app/app.module.ts
+++ jv-app/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Home } from './pages/home/home.component';
 import { Portfolio } from './pages/portfolio/portfolio.component';
+import { About } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Home,
-    Portfolio
+    Portfolio,
+    About
   ],
   imports: [
     BrowserModule,
---
+++ jv-app/src/app/pages/about/about.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { About } from './about.component';
+
+describe('About', () => {
+  let component: About;
+  let fixture: ComponentFixture<About>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ About ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(About);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
